4f3805cad4426322e96274a96d297eb00f0467e2
BrennanP01 <brennanjprice@lewisu.edu>
2021-03-23 15:07:27 -0500

    added number representation of dice roll

--- javaScript/rollDice.js
+++ javaScript/rollDice.js
@@ -24,11 +24,22 @@ function rollDice(){
 
 function cycle(){
     var angleClass;
-    document.getElementById("die1").src=images[randomInt(6)];
-    document.getElementById("die2").src=images[randomInt(6)];
-    document.getElementById("die3").src=images[randomInt(6)];
-    document.getElementById("die4").src=images[randomInt(6)];
-    document.getElementById("die5").src=images[randomInt(6)];
+    var die1Num = randomInt(6);
+    var die2Num = randomInt(6);
+    var die3Num = randomInt(6);
+    var die4Num = randomInt(6);
+    var die5Num = randomInt(6);
+    document.getElementById("die1").src=images[die1Num];
+    document.getElementById("die2").src=images[die2Num];
+    document.getElementById("die3").src=images[die3Num];
+    document.getElementById("die4").src=images[die4Num];
+    document.getElementById("die5").src=images[die5Num];
+
+    document.getElementById("die1Num").textContent=die1Num + 1;
+    document.getElementById("die2Num").textContent=die2Num + 1;
+    document.getElementById("die3Num").textContent=die3Num + 1;
+    document.getElementById("die4Num").textContent=die4Num + 1;
+    document.getElementById("die5Num").textContent=die5Num + 1;
 
     angleClass = angleClasses[randomInt(4)];
     document.getElementById("die1").className=angleClass;
